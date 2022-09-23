83f45f09d2e904bdc434900c0b41d1954827a6e7
Leonardo Tomazine <lt.altomacao@gmail.com>
2022-09-23 16:09:46 -0300

    atualizando repositorio

---
+++ aula07/src/main/java/com/mycompany/aula07/Luta.java
@@ -0,0 +1,14 @@
+package com.mycompany.aula07;
+
+public class Luta {
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    public void marcarLuta(){
+
+    }
+    public void Lutar() {
+    }
+    }
---
+++ untitled/src/main/java/org/example/Luta.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Luta {
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    public void marcarLuta(){
+
+    }
+    public void Lutar() {
+    }
+}
